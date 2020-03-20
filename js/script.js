f0788171657a351032ed561f90a3106bc68b6f7f
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-03-20 13:17:07 +0200

    Actualizare element css.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ function getCurrentInfo() {
     var current_date = new Date();
     var date_time = "Data:" + current_date.getDate() + "-" +
         (current_date.getMonth() + 1) + "-" +
-        current_date.getFullYear() + "; Ora:" +
+        current_date.getFullYear() + "<br /> Ora:" +
         current_date.getHours() + ":" +
         current_date.getMinutes() + ":" +
         current_date.getSeconds();
-    var current_url = " Link URL: " + window.location.href;
-    document.getElementById("section1").innerHTML(date_time);
+    var current_url = "<br /> Link URL: " + window.location.href;
+    document.getElementById("section1").innerHTML = document.getElementById("section1").innerHTML + date_time + current_url;
 }
