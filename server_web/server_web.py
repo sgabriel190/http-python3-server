08864944a6bd1173721bb30f5b3cd846ac6ef0e6
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-04-01 10:53:34 +0300

    Lab 7.

--- server_web/server_web.py
+++ server_web/server_web.py
@@ -1,71 +1,96 @@
 import socket
-import os
+import os # pentru dimensiunea fisierului
 
-CRLF = "\r\n"
 os.chdir("..")
-print(os.getcwd())
-
-def myReadFile(path):
-    f = open(path, "rb")
-    continut = f.read()
-    f.close()
-    return continut
-
-def formatHttpPacket(file, httpType, fileType):
-    response = ""
-    response += httpType + " 200  OK" + CRLF 
-    response += "Server: py_server" + CRLF
-    response += "Content-Type: " + fileType + CRLF
-    response += "Content-Length: " + str(len(file.encode('utf-8'))) + CRLF
-    response += file + CRLF + CRLF
-    return response
-
-def findAcceptField(packet):
-    packet_split = packet.split(CRLF)
-    accept_field = packet_split[3].find(',')
-    accept = packet_split[3][8:accept_field]
-    return accept
 
 # creeaza un server socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # specifica ca serverul va rula pe portul 5678, accesibil de pe orice ip al serverului
 serversocket.bind(('', 5678))
-
 # serverul poate accepta conexiuni; specifica cati clienti pot astepta la coada
 serversocket.listen(5)
 
 while True:
-    print("#########################################################################")
-    # asteapta conectarea unui client la server
-    print("Serverul asculta potentiali clienti.")
+	print('#########################################################################')
+	print('Serverul asculta potentiali clienti.')
+	# asteapta conectarea unui client la server
+	# metoda `accept` este blocanta => clientsocket, care reprezinta socket-ul corespunzator clientului conectat
+	(clientsocket, address) = serversocket.accept()
+	print('S-a conectat un client.')
+	# se proceseaza cererea si se citeste prima linie de text
+	cerere = ''
+	linieDeStart = ''
+	while True:
+		buf = clientsocket.recv(1024)
+		if len(buf) < 1:
+			break
+		cerere = cerere + buf.decode()
+		print('S-a citit mesajul: \n---------------------------\n' + cerere + '\n---------------------------')
+		pozitie = cerere.find('\r\n')
+		if (pozitie > -1 and linieDeStart == ''):
+			linieDeStart = cerere[0:pozitie]
+			print('S-a citit linia de start din cerere: ##### ' + linieDeStart + ' #####')
+			break
+	print('S-a terminat cititrea.')
+	if linieDeStart == '':
+		clientsocket.close()
+		print('S-a terminat comunicarea cu clientul - nu s-a primit niciun mesaj.')
+		continue
+	# interpretarea sirului de caractere `linieDeStart`
+	elementeLineDeStart = linieDeStart.split()
+	# TODO securizare
+	numeResursaCeruta = elementeLineDeStart[1]
+	
+	# calea este relativa la directorul de unde a fost executat scriptul
+	numeFisier = '../continut' + numeResursaCeruta
+	
+	fisier = None
+	try:
+		# deschide fisierul pentru citire in mod binar
+		fisier = open(numeFisier,'rb')
 
-    # metoda `accept` este blocanta => clientsocket, care reprezinta socket-ul corespunzator clientului conectat
-    (clientsocket, address) = serversocket.accept()
-    print("S-a conectat un client.")
+		# tip media
+		numeExtensie = numeFisier[numeFisier.rfind('.')+1:]
+		tipuriMedia = {
+			'html': 'text/html',
+			'css': 'text/css',
+			'js': 'application/js',
+			'png': 'image/png',
+			'jpg': 'image/jpeg',
+			'jpeg': 'image/jpeg',
+			'gif': 'image/gif',
+			'ico': 'image/x-icon',
+			'xml': 'application/xml',
+			'json': 'application/json'
+		}
+		tipMedia = tipuriMedia.get(numeExtensie,'text/plain')
+		
+		# se trimite raspunsul
+		clientsocket.sendall('HTTP/1.1 200 OK\r\n')
+		clientsocket.sendall('Content-Length: ' + str(os.stat(numeFisier).st_size) + '\r\n')
+		clientsocket.sendall('Content-Type: ' + tipMedia +'\r\n')
+		clientsocket.sendall('Server: My PW Server\r\n')
+		clientsocket.sendall('\r\n')
+		
+		# citeste din fisier si trimite la server
+		buf = fisier.read(1024)
+		while (buf):
+			clientsocket.send(buf)
+			buf = fisier.read(1024)
+	except IOError:
+		# daca fisierul nu exista trebuie trimis un mesaj de 404 Not Found
+		msg = 'Eroare! Resursa ceruta ' + numeResursaCeruta + ' nu a putut fi gasita!'
+		print(msg)
+		clientsocket.sendall('HTTP/1.1 404 Not Found\r\n')
+		clientsocket.sendall('Content-Length: ' + str(len(msg.encode('utf-8'))) + '\r\n')
+		clientsocket.sendall('Content-Type: text/plain\r\n')
+		clientsocket.sendall('Server: My PW Server\r\n')
+		clientsocket.sendall('\r\n')
+		clientsocket.sendall(msg)
 
-    # se proceseaza cererea si se citeste prima linie de text
-    cerere = ""
-    linieDeStart = ""
-    while True:
-        data = clientsocket.recv(1024)
-        cerere =  data.decode("utf-8")
-        print("S-a citit mesajul: \n---------------------------\n" + cerere + "\n---------------------------")
-        pozitie = cerere.find(CRLF)
-        accept_field = findAcceptField(cerere)
-        if (pozitie > -1):
-            linieDeStart = cerere[0:pozitie]
-            print("S-a citit linia de start din cerere: ##### " + linieDeStart + " #####")
-            break
-        else:
-            print("Cerere de la client invalida.")
-    print("S-a terminat cititrea.")
-    argument_list = linieDeStart.split(" ")
-    
-    response_file = myReadFile(argument_list[1][1:])
-    packet = formatHttpPacket(response_file, argument_list[2], accept_field)
-    
-    clientsocket.sendall(packet.encode("utf-8"))
+	finally:
+		if fisier is not None:
+			fisier.close()
+	clientsocket.close()
+	print('S-a terminat comunicarea cu clientul.')
 
-    clientsocket.close()
-    print("S-a terminat comunicarea cu clientul.")
